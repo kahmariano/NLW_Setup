314ee3c3e8c865fe0221a4a5d59710d65895949b
kahmariano <kmariano138@gmail.com>
2023-01-20 08:31:49 -0300

    web parte do front onde coloca os quadradinhos

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+//componente nada mais é do q uma forma de eu conseguir reaproveitar um pedaço de codigo
+
+// interface HabitDayProps {
+//   completed: number
+// }
+
+export function HabitDay() {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  )
+}

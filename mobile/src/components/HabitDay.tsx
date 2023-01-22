7bbd186f673ca99470a287da0b0411acbdf57ec8
kahmariano <kmariano138@gmail.com>
2023-01-22 16:39:25 -0300

    mobile 3 aula

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, TouchableOpacity, TouchableOpacityComponent,  } from "react-native"
+import { Dimensions, TouchableOpacity, TouchableOpacityProps,  } from "react-native"
 
 
 const WEEK_DAYS = 7
@@ -7,15 +7,16 @@ const SCREEN_HORIZONTAL_PADDING = (32*2) / 5;
 export const DAY_MARGIN_BETWWWN = 8;
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) -(SCREEN_HORIZONTAL_PADDING +5)
 
+interface Props extends TouchableOpacityProps {}
 
-
-export function HabitDay() {
+export function HabitDay({...rest}:Props) {
   return (
 
     <TouchableOpacity
     className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
    style={{width: DAY_SIZE, height: DAY_SIZE}} 
         activeOpacity={0.7}
+        {...rest}
     ></TouchableOpacity>
   )
 }

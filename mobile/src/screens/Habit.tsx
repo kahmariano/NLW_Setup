fe0421d289815c9e01f2c236394cc52390282cb9
kahmariano <kmariano138@gmail.com>
2023-01-23 00:37:23 -0300

    mobile 5 aula

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,13 +1,16 @@
 import { useRoute } from "@react-navigation/native";
+import clsx from "clsx";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { View, ScrollView, Text, Alert } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/Checkbox";
+import { HabitsEmpty } from "../components/HabitsEmpty";
 import {Loading}  from "../components/Loading";
 import { ProgressBar } from "../components/ProgressBar";
 import { api } from "../lib/axios";
-
+import { generateDateFromYearBeginnig } from "../utils/generate-ranger-betweem-dates";
+import {generetaProgressPercentage} from '../utils/generateProgressPercentage'
 interface Params {
   date:string;
 } 
@@ -21,7 +24,7 @@ interface DayInfoProps {
 }
 
  export function Habit() {
-
+const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
   const [completedHabits, setCompletedHabits] = useState<string[]> ([])
   const [dayInfo, setDayInfo] = useState<DayInfoProps | null >(null)
   const [Loading, setLoading] = useState(true)
@@ -31,6 +34,8 @@ interface DayInfoProps {
     const parsedDate = dayjs(date)
     const dayOfWeek = parsedDate.format('dddd')
     const dayAndMonth = parsedDate.format('DD/MM')
+
+    const habitsProgress = dayInfo?.possibleHabits.length ? generetaProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) : 0
 
 async function fetchHabits(){
   try {
@@ -87,25 +92,37 @@ if (loading) {
           {dayAndMonth}
         </Text>
 
-        <ProgressBar progress={30}/>
+        <ProgressBar progress={habitsProgress}/>
 
         <View 
-        className="mt-6"
+        className={clsx ("mt-6", {
+          ['opacity-50']:isDateInPast
+        })}
         >
        {
-        dayInfo?.possibleHabits &&
+        dayInfo?.possibleHabits ?
         dayInfo?.possibleHabits.map(habit => (
           <CheckBox
           key={habit.id}
           title={habit.title}
           checked={completedHabits.includes(habit.id)}
+          disabled={isDateInPast}
           onPress={() => {handleToggleHabit(habit.id)}}
             />
           
         ))
+        : <HabitsEmpty/>
        }
     
         </View>
+
+        {
+          isDateInPast && (
+            <Text className="text-white mt-10 text-center">
+              Você não pode alterar hábitos do passado
+            </Text>
+          )
+        }
 
       </ScrollView>
 
